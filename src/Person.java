308f88f2a5fb91668fdd65f6258da69cfe3652c3
rodrigoinfantozzi <r.infantozzi@isturin.it>
2022-01-23 23:09:51 +0100

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,79 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class Person {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private LocalDate dateOfBirth;
+
+
+    public Person(String name, String email, String phoneNumber, String address, LocalDate dateOfBirth) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Person(String name, String email, String phoneNumber, String address, String dateOfBirth) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.setDateOfBirth(dateOfBirth);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth){
+        this.dateOfBirth = LocalDate.parse(dateOfBirth, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    public int getAge(){
+        LocalDate today = LocalDate.now();
+        Period period = Period.between(this.dateOfBirth, today);
+        return period.getYears();
+    }
+
+}
---
+++ src/Screen.java
@@ -0,0 +1,154 @@
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class Screen extends JFrame {
+    private JPanel panelMain;
+    private JPanel panelTop;
+    private JPanel panelLeft;
+    private JPanel panelRight;
+    private JList listPeople;
+    private JTextField textName;
+    private JTextField textEmail;
+    private JTextField textPhoneNumber;
+    private JTextField textAddress;
+    private JTextField textDOB;
+    private JButton buttonNew;
+    private JButton buttonSave;
+    private JLabel labelAge;
+    private JButton buttonClear;
+
+    private ArrayList<Person> people;
+    private DefaultListModel listPeopleModel;
+
+    Screen() {
+        super("My Contacts Project");
+        this.setContentPane(this.panelMain);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        people = new ArrayList<Person>();
+        listPeopleModel = new DefaultListModel();
+        listPeople.setModel(listPeopleModel);
+        buttonSave.setEnabled(false);
+
+        buttonNew.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                buttonNewClick(e);
+            }
+        });
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                buttonSavedClick(e);
+            }
+        });
+        listPeople.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                listPeopleSelection(e);
+            }
+        });
+
+
+        buttonClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                buttonClearClick(e);
+            }
+        });
+    }
+
+    public void buttonClearClick(ActionEvent e){
+        textName.setText(" ");
+        textEmail.setText(" ");
+        textPhoneNumber.setText(" ");
+        textAddress.setText(" ");
+        textDOB.setText(" ");
+
+    }
+
+    public void buttonNewClick(ActionEvent e) {
+        String DOB = " ";
+        if (DOB.equals(textDOB.getText()))
+            DOB = "01/01/2000";
+        else
+            DOB = textDOB.getText();
+
+        Person p = new Person(
+                textName.getText(),
+                textEmail.getText(),
+                textPhoneNumber.getText(),
+                textAddress.getText(),
+                DOB
+        );
+        people.add(p);
+        refreshPeopleList();
+    }
+
+    public void buttonSavedClick(ActionEvent e) {
+        int personNumber = listPeople.getSelectedIndex();
+        String empty = " ";
+        if (personNumber >= 0) {
+            Person p = people.get(personNumber);
+            p.setName(textName.getText());
+            p.setEmail(textEmail.getText());
+            p.setPhoneNumber(textPhoneNumber.getText());
+            p.setAddress(textAddress.getText());
+            if (empty.equals(textDOB.getText()))
+                p.setDateOfBirth("01/01/2000");
+            else
+                p.setDateOfBirth(textDOB.getText());
+            refreshPeopleList();
+        }
+    }
+
+    public void listPeopleSelection(ListSelectionEvent e) {
+        int personNumber = listPeople.getSelectedIndex();
+        if ( personNumber >= 0){
+            Person p = people.get(personNumber);
+            textName.setText(p.getName());
+            textEmail.setText(p.getEmail());
+            textPhoneNumber.setText(p.getPhoneNumber());
+            textAddress.setText(p.getAddress());
+            textDOB.setText(p.getDateOfBirth().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+            labelAge.setText((Integer.toString(p.getAge()) + " years"));
+            buttonSave.setEnabled(true);
+        }
+        else
+            buttonSave.setEnabled(false);
+    }
+
+    public void refreshPeopleList() {
+        listPeopleModel.removeAllElements();
+        System.out.println("Removing all people from list");
+        for (Person p : people){
+            System.out.println("Adding person to list: " + p.getName() );
+            listPeopleModel.addElement(p.getName());
+        }
+
+    }
+
+    public void addPerson(Person p){
+        people.add(p);
+        refreshPeopleList();
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Screen screen = new Screen();
+        screen.setVisible(true);
+
+        Person infantozzi = new Person("Mr Infantozzi", "r.infantozzi@isturin.it", "391 999 9999", "Via Roma 999", "22/09/1979" );
+
+        screen.addPerson(infantozzi);
+
+    }
+}
